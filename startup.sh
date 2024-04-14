f6213cf1b9d4c9513206ba9a4c4f8fb72689a405
Michael Ahamefule <chinedumike2010@gmail.com>
2024-04-13 19:34:46 -0700

    third commit

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install nginx -y 
 sudo systemctl enable nginx
 sudo systemctl start nginx
-git clone https://git.toptal.com/screening-ops/Anthony-ahamefule.git
-cd /Anthony-ahamefule
+git clone https://github.com/mahamefule/miichael-ahamefule-devsecops-project.git
+cd /Michael-ahamefule
 pip install requirements.txt
 echo "*/10 * * * * $SHELL remediation.sh" | crontab -
